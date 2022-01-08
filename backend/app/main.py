42ebf967436d1392c43a3946752213d5df379ab6
Kenta Yamada <kentayamadadev@gmail.com>
2022-01-08 04:21:27 +0000

    install rate limit

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,33 @@
+import sys
+
 from fastapi import FastAPI, exceptions
 from fastapi.middleware.cors import CORSMiddleware
+from slowapi import Limiter  # type: ignore
+from slowapi import _rate_limit_exceeded_handler  # type: ignore
+from slowapi.errors import RateLimitExceeded
+from slowapi.middleware import SlowAPIMiddleware
+from slowapi.util import get_remote_address
 
 from app.core.error.http_exception import custom_http_exception_handler
 from app.core.error.validation import custom_validation_exception_handler
-from app.core.schema.config import settings
 from app.db.database import create_table
-from app.router import auth, image, item, seller, root
+from app.router import auth, image, item, root, seller
 
 create_table()
 
 app = FastAPI(title="Mercari Clone API")
 
+
+if "pytest" in sys.modules:
+    limiter = Limiter(key_func=get_remote_address, enabled=False)
+else:
+    limiter = Limiter(
+        key_func=get_remote_address, default_limits=["10/minute"], enabled=True
+    )
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
+app.add_middleware(SlowAPIMiddleware)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
