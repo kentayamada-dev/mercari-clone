0a97636e651b2bd0198c5e6478f55a1d67f946be
Kenta Yamada <kentayamadadev@gmail.com>
2022-02-05 13:01:12 +0000

    update seed

--- backend/app/db/seed.py
+++ backend/app/db/seed.py
@@ -29,15 +29,16 @@ def seed() -> None:
         user.items = [
             Item(
                 name=name + fake.text(max_nb_chars=10),
-                price=random.randint(500, 99999),
+                price=random.randint(500, 9999),
                 description=fake.sentence(nb_words=10),
                 user_id=user.id,
                 image_url=f"https://i.pravatar.cc/150?img={random.randint(40, 60)}",  # type: ignore
             )
-            for _ in range(random.randint(0, 5))
+            for _ in range(random.randint(0, 10))
         ]
         user.saved_queries = [
-            Query(query=fake.word(), user_id=user.id) for _ in range(random.randint(0, 5))
+            Query(query=fake.word(), user_id=user.id)
+            for _ in range(random.randint(0, 5))
         ]
         objects.append(user)
     db.add_all(objects)
